b75914f943d03caf82124ff95328f2b937dc6b1f
chyneyee <windyspring@hotmail.com>
2015-03-13 07:41:09 +0800

    Re-correct the code

--- arguments/numargs.sh
+++ arguments/numargs.sh
@@ -2,11 +2,19 @@
 
 #Change the args script supplied earlier so that if no argument is provided, "They are" isn't printed, and if exactly 1 argument is provided, "... 1 argument" rather than "... 1 arguments" is printed (use if)
 
-if [ $# -eq 1 ]; then
-echo "They are $# argument."
+if [ $# -eq 1 ]
+then
+string="It is "
+ending=""
 
-else 
-if [ $# -gt 1 ]; then
-echo "They are $# arguments."
+else
+string="They are "
+ending="s"
+fi 
+
+echo This $0 command has $# argument${ending}.
+
+if [ $# != 0 ]
+then
+echo $string $*
 fi
-fi	
